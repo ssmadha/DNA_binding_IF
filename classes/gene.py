1a782b76dd422b369a7700740663f297a564fd23
Shariq <ssmadha@hotmail.com>
2025-12-01 15:52:17 -0500

    Continue building gene.py

--- classes/gene.py
+++ classes/gene.py
@@ -1,6 +1,7 @@
 
 import ensembl_rest
 import mygene
+import re
 
 
 class Domain:
@@ -8,9 +9,8 @@ class Domain:
     Domain object
     """
 
-    def __init__(self, interpro_id, start, end, source, classification = None):
+    def __init__(self, interpro_id, start, end, source, **kwargs):
         self.interpro_id = interpro_id
-        self.classification = classification
         self.start = start
         self.end = end
         self.source = source
@@ -23,20 +23,25 @@ class Transcript:
     uniprot_id = None
     domains = None
 
-    def __init__(self, ensembl_id: str):
-        self.ensembl_id = ensembl_id
+    def __init__(self, enst_id: str, ensp_id: str):
+        self.enst_id = enst_id
+        self.ensp_id = ensp_id
         self.seq = self.download_sequence()
+        self.domains = self.download_domains()
 
-    def download_sequence(self, ensembl_id=None):
-        if ensembl_id==None:
-            ensembl_id = self.ensembl_id
-        seq = ensembl_rest.sequence_id(ensembl_id)
+    def download_sequence(self, enst_id=None):
+        if enst_id==None:
+            enst_id = self.enst_id
+        seq = ensembl_rest.sequence_id(enst_id)
         return seq
 
-    def download_domains(self):
-        results = ensembl_rest.overlap_translation(self.ensembl_id, 
+    def download_domains(self, ensp_id=None):
+        if ensp_id is None:
+            ensp_id = self.ensp_id
+        results = ensembl_rest.overlap_translation(ensp_id,
                                                    type="domain")
-        self.domains = [res for res in results]
+        #print(results)
+        return [Domain(interpro_id=res["interpro"], source = res["type"], **res) for res in results]
 
 class Gene:
     """
@@ -44,9 +49,11 @@ class Gene:
     """
     seq = None
     uniprot_id = None
+    refseq_id = None
     transcripts = None
+    superisoform_seq = None
     
-    def __init__(self, ensembl_id: str, biotype_filter=None):
+    def __init__(self, ensg_id: str, biotype_filter=None):
         """
         Initialize a Gene object based on Ensembl ID
 
@@ -55,17 +62,28 @@ class Gene:
         """
         if biotype_filter is None:
             biotype_filter = ['protein_coding']
-        self.ensembl_id = ensembl_id
-        self.transcripts = self.download_transcripts(self.ensembl_id, biotype_filter)
-        self.uniprot_id = self.download_uniprot_id()
+        self.ensg_id = ensg_id
+        self.gene_info = self.download_gene_info()
+        self.uniprot_id, self.refseq_id = self.check_alternate_id()
+        self.start_pos, self.end_pos, self.strand = self.check_positions()
+        self.transcripts = self.download_transcripts(self.ensg_id, biotype_filter)
+        self.check_domain_redundancy()
+        self.superisoform_seq = self.generate_superisofrom()
 
-    def download_transcripts(self, ensembl_gene_id=None, biotype_filter=None):
+    def download_gene_info(self, ensg_id=None):
+        if ensg_id is None:
+            ensg_id = self.ensg_id
+        mg = mygene.MyGeneInfo()
+        get_gene_result = mg.getgene(ensg_id)
+        return get_gene_result
+
+    def download_transcripts(self, ensg_id=None, biotype_filter=None):
         if biotype_filter is None:
             biotype_filter = ['protein_coding']
-        if ensembl_gene_id is None:
-            ensembl_gene_id = self.ensembl_id
+        if ensg_id is None:
+            ensg_id = self.ensg_id
         try:
-            isoforms = ensembl_rest.lookup(ensembl_gene_id,
+            isoforms = ensembl_rest.lookup(ensg_id,
                                            params={'multiple_sequences': True,
                                                    'type': 'protein',
                                                    'expand': True
@@ -75,9 +93,9 @@ class Gene:
             error_code = err.response.status_code
             error_message= err.response.json()['error']
             if(error_code==400) and ("not found" in error_message):
-                print(ensembl_gene_id + " not found")
+                print(ensg_id + " not found")
             elif(error_code==400) and ("No sequences returned" in error_message):
-                print(ensembl_gene_id + " no protein sequences found")
+                print(ensg_id + " no protein sequences found")
             else:
                 raise
             return None
@@ -85,18 +103,43 @@ class Gene:
         for isoform in isoforms["Transcript"]:
             if isoform['biotype'] not in biotype_filter:
                 continue
-            transcript = Transcript(isoform['id'])
+            transcript = Transcript(isoform['id'], isoform['Translation']['id'])
             transcripts.append(transcript)
         return transcripts
 
-    def download_uniprot_id(self, ensembl_id = None):
-        if ensembl_id is None:
-            ensembl_id = self.ensembl_id
-        mg = mygene.MyGeneInfo()
-        get_gene_result = mg.getgene(ensembl_id)
-        print(get_gene_result)
+    def check_alternate_id(self, ensg_id = None):
+        if ensg_id is None:
+            ensg_id = self.ensg_id
+        gene_info = self.gene_info
+        print(gene_info['refseq'])
         uniprot_id = None
-        if get_gene_result is not None and "uniprot" in get_gene_result:
-            uniprot_id = get_gene_result["uniprot"]
-        print("No UniProt ID found for " + ensembl_id)
-        return uniprot_id
+        refseq_id = None
+        if gene_info is not None:
+            if "uniprot" in gene_info:
+                uniprot_id = gene_info["uniprot"]
+            if "refseq" in gene_info:
+                refseq_id = list(filter(re.compile("NG_.*").match, gene_info["refseq"]["genomic"][0]))
+        if uniprot_id is None:
+            print("No UniProt ID found for " + ensg_id)
+        return uniprot_id, refseq_id
+
+    def check_positions(self):
+        gene_info = self.gene_info
+        if type(gene_info['genomic_pos']) is list:
+            start_pos = gene_info['genomic_pos'][0]['start']
+            end_pos = gene_info['genomic_pos'][0]['end']
+            strand = gene_info['genomic_pos'][0]['strand']
+        else:
+            start_pos = gene_info['genomic_pos']['start']
+            end_pos = gene_info['genomic_pos']['end']
+            strand = gene_info['genomic_pos']['strand']
+        return start_pos, end_pos, strand
+
+    def check_domain_redundancy(self, transcripts=None):
+        if transcripts is None:
+            transcripts = self.transcripts
+        
+
+    def generate_superisofrom(self):
+        pass
+
--- tests/object_tests.py
+++ tests/object_tests.py
@@ -2,12 +2,22 @@ import unittest
 import classes.gene as gene
 
 class TestGene(unittest.TestCase):
+    test_ensg_id = "ENSG00000004848"
+    test_gene = gene.Gene(test_ensg_id)
     def test_Gene_creation(self):
-        test_ensembl_id = "ENSG00000179456"
-        test_gene = gene.Gene(test_ensembl_id)
-        self.assertEqual(test_ensembl_id, test_gene.ensembl_id)  # add assertion here
-        self.assertGreater(len(test_gene.download_transcripts()), 0)
-        self.assertIsNotNone(test_gene.uniprot_id)
+        self.assertEqual(self.test_ensg_id, self.test_gene.ensg_id)  # add assertion here
+        self.assertGreater(len(self.test_gene.download_transcripts()), 0)
+        self.assertIsNotNone(self.test_gene.uniprot_id)
+        self.assertIsNotNone(self.test_gene.refseq_id)
+        self.assertGreater(self.test_gene.start_pos, 0)
+        self.assertGreater(self.test_gene.end_pos, self.test_gene.start_pos)
+        self.assertIn(self.test_gene.strand, [1, -1])
+
+    def test_Transcript_creation(self):
+        self.assertEqual(self.test_gene.transcripts[0].enst_id[:4], "ENST")
+        self.assertEqual(self.test_gene.transcripts[0].ensp_id[:4], "ENSP")
+        self.assertGreater(len(self.test_gene.transcripts[0].domains), 0)
+        self.assertIsNotNone(self.test_gene.transcripts[0].domains[0].interpro_id)
 
 if __name__ == '__main__':
     unittest.main()
