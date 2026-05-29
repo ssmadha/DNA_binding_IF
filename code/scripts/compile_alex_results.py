5717c4cf71d919b292661f1646e36b9172f2f9ba
Shariq <ssmadha@hotmail.com>
2026-05-29 12:05:26 -0400

    Adding compile_alex_results.py

---
+++ code/scripts/compile_alex_results.py
@@ -0,0 +1,39 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+alpha = 63
+beta = 0.3
+
+coverage_df = pd.read_csv("results_17k/results/individual/all_results.txt", sep='\t', header=None)
+coverage_df.columns = ["gene_id", "transcript_id", "n_domains", "domain_coverage"]
+TPM_df = pd.read_csv("expressed_coding_isoforms_with_relative_tpm_threshold_1.tsv", sep='\t')
+
+full_df = pd.merge(coverage_df, TPM_df)
+
+def sigmoid(x):
+    return 1/(1+np.power(np.e,-x))
+
+asif_df = None
+for i in range(full_df.shape[0]):
+    curr_row = full_df.iloc[i, :]
+    new_row = curr_row[:4].copy()
+    coverages = [float(el) for el in curr_row["domain_coverage"][1:-1].split(", ")]
+    impact_factor = 0
+    for domain_coverage in coverages:
+        # print(isoform_coverage_percentages[gene_id][transcript.protein][domain])
+        # print(sigmoid(alpha*(1-isoform_coverage_percentages[gene_id][transcript.protein][domain]) \
+        #                            + beta))
+        impact_factor += sigmoid(alpha * (domain_coverage - beta))
+    impact_factor /= curr_row["n_domains"]
+    impact_factor = 1 - impact_factor
+    for tpm in curr_row[4:]:
+        tissue = curr_row.index[int(len(new_row)/2) + 2].split("_")[0]
+        new_row = pd.concat([new_row, pd.Series({tissue+"_tpm": tpm, tissue+"_asif": impact_factor*tpm})])
+    if asif_df is None:
+        asif_df = pd.DataFrame([new_row])
+    else:
+        asif_df = pd.concat([asif_df, pd.DataFrame([new_row])])
+
+asif_df.to_csv("PPI_ASIF_table1.tsv", sep='\t', index=False)
