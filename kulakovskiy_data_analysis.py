3471792f80c9d88db24510440e8fecf3040e1e9c
Shariq <ssmadha@hotmail.com>
2026-06-24 14:41:29 -0400

    Updated kulakovskiy_data_analysis.py

--- kulakovskiy_data_analysis.py
+++ kulakovskiy_data_analysis.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 maradoner_df = pd.read_table("kulakovskiy_data/MARAdoner_activities.tsv", index_col=0)
 maradoner_df = maradoner_df.set_index(maradoner_df.index.map(lambda x: x.split(".")[0]), append=True)
 maradoner_df.index = maradoner_df.index.set_names(["MotifID", "GeneID"])
 
-motif_info = pd.read_excel("kulakovskiy_data/SupplementaryTable11_MARA_Clusters.v2026.2.xlsx", sheet_name="TableS11")
+motif_info = pd.read_table("kulakovskiy_data/SupplementaryTable11_MARA_Clusters.v2026.2.tsv")
 
-asif_df = pd.read_table("ASIF_byGene_wName.csv", sep=",", index_col=[0,1])
+asif_df = pd.read_table("visualization/asif/ASIF.csv", sep=",", index_col=[0,1])
 
 common_columns = {"breast": "breast_adult",
                   "epididymis": "epididymis_adult",
@@ -48,13 +49,53 @@ asif_df_common = asif_df_common.rename(columns=common_columns)
 
 maradoner_df_common = maradoner_df.loc[pd.IndexSlice[:, common_genes], common_columns.values()]
 
-unique_motifs = motif_info.loc[motif_info["Cluster_Size"]==1, "Representative_Motif"].map(lambda x: x.split(".")[0]).tolist()
+corrs = dict()
+for gene in common_genes:
+    mask_asif = asif_df_common.index.get_level_values("Gene Name").isin([gene])
+    mask_maradoner = maradoner_df_common.index.get_level_values("GeneID").isin([gene])
+    print(gene)
+    x = asif_df_common.loc[mask_asif, :].to_numpy().ravel()
+    y = maradoner_df_common.loc[mask_maradoner, :].mean(axis=0).to_numpy()
 
-corrs = []
-for val in common_columns.values():
-    print(val)
-    corr = asif_df_common.loc[pd.IndexSlice[:, unique_motifs], val].reset_index(drop=True).corr(maradoner_df_common[pd.IndexSlice[:, unique_motifs], val].reset_index(drop=True))
-    corrs.append(asif_df_common[val].reset_index(drop=True).corr(maradoner_df_common[val].reset_index(drop=True)))
+    # Remove NaN pairs
+    valid = ~(pd.isna(x) | pd.isna(y))
+    x = x[valid]
+    y = y[valid]
+
+    corr = pd.Series(x).corr(pd.Series(y))
+    corrs[gene] = corr
+
+    print(f"{gene}: {corr:.3f}")
+
+    plt.figure(figsize=(5, 5))
+    plt.scatter(x, y)
+    plt.xlabel("Asif")
+    plt.ylabel("Maradoner (mean)")
+    plt.title(f"{gene}\nPearson r = {corr:.3f}")
+    plt.tight_layout()
+
+    # Save plot
+    plt.savefig(f"kulakovskiy_data/corr_graphs/{gene}_scatter.png", dpi=300, bbox_inches="tight")
+
 
 corrs = np.array(corrs)
-corrs.mean()
+print(corrs.mean())
+
+unique_motifs = motif_info.loc[motif_info["Cluster_Size"]<2, "Representative_Motif"].map(lambda x: x.split(".")[0]).tolist()
+
+unique_corrs = {gene: corrs[gene] for gene in unique_motifs if gene in common_genes}
+unique_corrs = []
+for gene in common_genes:
+    mask_asif = asif_df_common.index.get_level_values("Gene Name").isin(unique_motifs)
+    mask_maradoner = maradoner_df_common.index.get_level_values("GeneID").isin(unique_motifs)
+
+    print(gene)
+    x = asif_df_common.loc[mask_asif, :].to_numpy().ravel()
+    y = maradoner_df_common.loc[mask_maradoner, :].to_numpy().ravel()
+
+    corr = pd.Series(x).corr(pd.Series(y))
+    print(corr)
+    unique_corrs.append(corr)
+
+unique_corrs = np.array(unique_corrs)
+print(unique_corrs.mean())
